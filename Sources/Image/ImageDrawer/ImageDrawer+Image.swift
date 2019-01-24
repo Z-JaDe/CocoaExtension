a06760ac3ff67dc5d9226430903bcfea2c0f55a7
郑军铎 <zjade@outlook.com>
2019-01-24 16:51:56 +0800

    小修改

--- Sources/Image/ImageDrawer/ImageDrawer+Image.swift
+++ Sources/Image/ImageDrawer/ImageDrawer+Image.swift
@@ -9,23 +9,72 @@
 import Foundation
 
 public extension UIImage {
-    public class func create(width: CGFloat, height: CGFloat) -> ImageDrawer {
+    public static func create(width: CGFloat, height: CGFloat) -> ImageDrawer {
         return self.create(CGSize(width: width, height: height))
     }
-    public class func create(_ size: CGSize) -> ImageDrawer {
+    public static func create(_ size: CGSize) -> ImageDrawer {
         let drawer = ImageDrawer()
         drawer.size = .fixed(size)
         return drawer
     }
-    public class func createResizable() -> ImageDrawer {
+    public static func createResizable() -> ImageDrawer {
         let drawer = ImageDrawer()
         drawer.size = .resizable
         return drawer
     }
-    public class func create(color: UIColor?) -> UIImage {
+    public static func create(color: UIColor?) -> UIImage {
         return createResizable().color(color).image
     }
-    public class func create(color: UIColor?, _ size: CGSize) -> UIImage {
+    public static func create(color: UIColor?, _ size: CGSize) -> UIImage {
         return create(size).color(color).image
+    }
+}
+// MARK: - gradient
+extension UIImage {
+    public enum GradientDirection {
+        case topToBottom
+        case bottomToTop
+        case leftToRight
+        case rightToLeft
+    }
+    private static func calculatePoints(_ direction: GradientDirection) -> (CGPoint, CGPoint) {
+        let startPoint: CGPoint
+        let endPoint: CGPoint
+        switch direction {
+        case .topToBottom:
+            startPoint = CGPoint(x: 0.5, y: 0)
+            endPoint = CGPoint(x: 0.5, y: 1)
+        case .bottomToTop:
+            startPoint = CGPoint(x: 0.5, y: 1)
+            endPoint = CGPoint(x: 0.5, y: 0)
+        case .leftToRight:
+            startPoint = CGPoint(x: 0, y: 0.5)
+            endPoint = CGPoint(x: 1, y: 0.5)
+        case .rightToLeft:
+            startPoint = CGPoint(x: 1, y: 0.5)
+            endPoint = CGPoint(x: 0, y: 0.5)
+        }
+        return (startPoint, endPoint)
+    }
+    // MARK: -
+    public static func horizontalGradient(_ size: CGSize? = nil, colors: UIColor...) -> UIImage {
+        return gradient(.leftToRight, size, colors: colors)
+    }
+    public static func verticalGradient(_ size: CGSize? = nil, colors: UIColor...) -> UIImage {
+        return gradient(.leftToRight, size, colors: colors)
+    }
+    public static func gradient(_ direction: GradientDirection, _ size: CGSize? = nil, colors: UIColor...) -> UIImage {
+        return gradient(direction, size, colors: colors)
+    }
+    public static func gradient(_ direction: GradientDirection, _ size: CGSize? = nil, colors: [UIColor]) -> UIImage {
+        let drawer: ImageDrawer
+        if let size = size {
+            drawer = create(size)
+        } else {
+            drawer = createResizable()
+        }
+        let (startPoint, endPoint) = calculatePoints(direction)
+        let locations = colors.enumerated().map({CGFloat($0.offset) / CGFloat(colors.count - 1)})
+        return drawer.color(gradient: colors, locations: locations, from: startPoint, to: endPoint).image
     }
 }
