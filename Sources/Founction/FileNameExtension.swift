1a1bac0c835401acf35fb4fb3963d2d6d95c8474
zjade <zjade@outlook.com>
2020-01-17 15:49:04 +0800

    FileNameExtension

---
+++ Sources/Founction/FileNameExtension.swift
@@ -0,0 +1,50 @@
+//
+//  FileNameExtension.swift
+//  CocoaExtension
+//
+//  Created by Apple on 2020/1/17.
+//  Copyright © 2020 zjade. All rights reserved.
+//
+
+import Foundation
+
+/// 获取文件名称和扩展
+struct FileNameExtension {
+    let name: String
+    let exten: String
+    init(name: String, defaultExten: String) {
+        if let point = name.lastIndex(of: ".") {
+            let exten = String(name.suffix(from: name.index(after: point)))
+            self.exten = exten.isEmpty ? defaultExten : exten
+            self.name = String(name.prefix(upTo: point))
+        } else {
+            self.exten = defaultExten
+            self.name = name
+        }
+    }
+    @inline(__always)
+    func getData(_ path: String?) -> Data? {
+        guard let path = path else { return nil }
+        return try? Data(contentsOf: URL(fileURLWithPath: path))
+    }
+    @inline(__always)
+    func getPath() -> String? {
+        _getPath(name: name)
+    }
+    @inline(__always)
+    func getImagePath() -> String? {
+        let scale = UIScreen.main.scale
+        if scale > 1.0 {
+            if let path = _getPath(name: "\(name)@2x") {
+                return path
+            } else if let path = _getPath(name: "\(name)@3x") {
+                return path
+            }
+        }
+        return _getPath(name: name)
+    }
+    @inline(__always)
+    private func _getPath(name: String) -> String? {
+        Bundle.main.path(forResource: name, ofType: exten)
+    }
+}
--- Sources/Image/CGImageSource/UIImage+GIF.swift
+++ Sources/Image/CGImageSource/UIImage+GIF.swift
@@ -30,30 +30,8 @@ public extension UIImage {
         return UIImage.animatedImage(with: result.images, duration: result.duration)
     }
     static func animatedGIF(withName name: String) -> UIImage? {
-        let imageExtension: String
-        let imageName: String
-        if let point = name.lastIndex(of: ".") {
-            imageExtension = String(name.suffix(from: name.index(after: point)))
-            imageName = String(name.prefix(upTo: point))
-        } else {
-            imageExtension = ""
-            imageName = name
-        }
-        func getData(name: String) -> Data? {
-            guard let path = Bundle.main.path(forResource: name, ofType: imageExtension.isEmpty ? "gif" : imageExtension) else {
-                return nil
-            }
-            return try? Data(contentsOf: URL(fileURLWithPath: path))
-        }
-        let scale = UIScreen.main.scale
-        if scale > 1.0 {
-            if let data = getData(name: "\(imageName)@2x") {
-                return animatedGIF(withData: data)
-            } else if let data = getData(name: "\(imageName)@3x") {
-                return animatedGIF(withData: data)
-            }
-        }
-        if let data = getData(name: imageName) {
+        let file = FileNameExtension(name: name, defaultExten: "gif")
+        if let data = file.getData(file.getImagePath()) {
             return animatedGIF(withData: data)
         }
         return UIImage(named: name)
