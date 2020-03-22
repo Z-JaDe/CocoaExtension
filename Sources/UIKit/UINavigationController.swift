379e39ed1b8b907ff9c8e07c69d062b6dcd42cd9
zjade <zjade@outlook.com>
2020-03-22 17:12:06 +0800

    bug修改

--- Sources/UIKit/UINavigationController.swift
+++ Sources/UIKit/UINavigationController.swift
@@ -19,14 +19,24 @@ extension UINavigationController {
         }
     }
     public func pop(count: Int, animated: Bool) {
-        var vcArr = self.viewControllers
-        vcArr.removeLast(count)
-        self.setViewControllers(vcArr, animated: animated)
+        if count <= 0 { return }
+        let viewConArr = self.viewControllers.dropLast(count)
+        if viewConArr.isEmpty {
+            // 只保留根控制器，如果本来就只有1个根控制器则不需要设置
+            if let first = self.viewControllers.first, self.viewControllers.count > 1 {
+                self.setViewControllers([first], animated: animated)
+            }
+        } else {
+            self.setViewControllers(Array(viewConArr), animated: animated)
+        }
     }
     public func popAndPush(count: Int, pushVC: UIViewController, animated: Bool) {
-        var vcArr = self.viewControllers
-        vcArr.removeLast(count)
-        vcArr.append(pushVC)
-        self.setViewControllers(vcArr, animated: animated)
+        if count <= 0 {
+            self.pushViewController(pushVC, animated: animated)
+        } else {
+            var viewConArr: [UIViewController] = self.viewControllers.dropLast(count)
+            viewConArr.append(pushVC)
+            self.setViewControllers(viewConArr, animated: animated)
+        }
     }
 }
